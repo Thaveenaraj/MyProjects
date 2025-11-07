1755e8b34cd11d8ec920d9f1340cd48cf11f00ae
Thaveena Raj <thaveenaraj@Yuvarajs-MacBook-Air.local>
2025-11-08 02:34:05 +0530

    My RDS CI/CD Project Code

---
+++ src/main/java/com/example/rds_terraform_project/RdsTerraformProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rds_terraform_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdsTerraformProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdsTerraformProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rds_terraform_project/RdsTerraformProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rds_terraform_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdsTerraformProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
